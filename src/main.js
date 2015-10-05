27ffb040bbf82d7411cfa243bda65daeb0f8d391
Alexey Novak <alexey.novak.mail@gmail.com>
2015-10-04 22:10:46 -0400

    adding header and components

--- src/main.js
+++ src/main.js
@@ -1,5 +1,36 @@
 $ = jQuery = require('jquery');
 
-var App = console.log('Hello world');
+var React = require('react'),
+    Home = require('./components/homePage.js'),
+    About = require('./components/about/aboutPage.js'),
+    Header = require('./components/common/header.js');
 
-module.exports = App;
+(function(win) {
+  'use strict';
+
+  var App = React.createClass({
+    render: function() {
+      var Child;
+
+      switch(this.props.route) {
+        case 'about': Child = About; break;
+        default: Child = Home;
+      }
+
+      return (
+        <div>
+          <Header/>
+          <Child/>
+        </div>
+      );
+    }
+  });
+
+  function render() {
+    var route = win.location.hash.substr(1);
+    React.render(<App route={route} />, document.getElementById('app'));
+  };
+
+  win.addEventListener('hashchange', render);
+  render();
+})(window);
